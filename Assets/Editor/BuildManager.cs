bd5d812f7bd7f6291e8ff4c019507ae1d11d2b85
coco1337 <scar3517@gmail.com>
2021-07-26 14:14:26 +0900

    빌드설정

---
+++ Assets/Editor/BuildManager.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+  [MenuItem("Build/Build WebGL")]
+  public static void Build()
+  {
+    var buildPlayerOptions = new BuildPlayerOptions
+    {
+      scenes = EditorBuildSettings.scenes.Select(scene => scene.path).ToArray(),
+      target = BuildTarget.WebGL,
+      options = BuildOptions.None,
+      locationPathName = "Builds/WebGL/"
+    };
+
+    var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+    var buildSum = report.summary;
+    
+    switch (buildSum)
+    {
+      case { result: BuildResult.Succeeded }:
+        Debug.Log("Build Succeeded: " + buildSum.totalSize + " bytes");
+        break;
+      case { result: BuildResult.Failed }:
+        Debug.Log("Build failed");
+        break;
+    }
+  }
+}
